3e847a4d354a9f756f6e59ae451cfa53ede9c922
Ben Meyer <unboundmusic@me.com>
2015-11-19 19:50:24 -0800

    Initial Commit

---
+++ VoiceCode Monitor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VoiceCode Monitor
+//
+//  Created by Benjamin Meyer on 11/19/15.
+//  Copyright © 2015 VoiceCode. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ VoiceCode MonitorTests/VoiceCode_MonitorTests.swift
@@ -0,0 +1,36 @@
+//
+//  VoiceCode_MonitorTests.swift
+//  VoiceCode MonitorTests
+//
+//  Created by Benjamin Meyer on 11/19/15.
+//  Copyright © 2015 VoiceCode. All rights reserved.
+//
+
+import XCTest
+@testable import VoiceCode_Monitor
+
+class VoiceCode_MonitorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
